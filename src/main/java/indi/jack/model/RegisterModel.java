c82bca1a4ed997c5b63e26f33b97889a79cf08f0
huangyong <huangyong@eetown.com>
2019-08-01 12:01:57 +0800

    update .gitignore

--- src/main/java/indi/jack/model/RegisterModel.java
+++ src/main/java/indi/jack/model/RegisterModel.java
@@ -4,6 +4,7 @@
 package indi.jack.model;
 
 import indi.jack.entity.AccountInfo;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * <p>Title:RegisterModel用户注册模型</p>
@@ -16,30 +17,36 @@ public class RegisterModel {
 	/*@NotBlank(message="{validate.register.form.username.notblank}")*/
 	/*@Pattern(regexp = "^[0-9]*$",message="{validate.register.form.username.onlydigit}")*/
 	/*@NotNull(message="{validate.register.form.username.notblank}")*/
-	private Long userId;
+	private Long userId = 20190801l;
 	/*@NotBlank(message="{validate.register.form.password.notblank}")*/
-	private String password;
+	private String password = "123456";
 	/*@NotNull(message="{validate.register.form.username.notblank}")*/
-	private int roleid;
+	private int roleid = 1;
 	private AccountInfo accountInfo;
 
 	public Long getUserId() {
 		return userId;
 	}
 	public void setUserId(Long userId) {
-		this.userId = userId;
+		if (null != userId) {
+			this.userId = userId;
+		}
 	}
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
-		this.password = password;
+		if (!StringUtils.isEmpty(password)) {
+			this.password = password;
+		}
 	}
 	public int getRoleid() {
 		return roleid;
 	}
 	public void setRoleid(int roleid) {
-		this.roleid = roleid;
+		if (roleid > 0) {
+			this.roleid = roleid;
+		}
 	}
 	public AccountInfo getAccountInfo() {
 		return accountInfo;
